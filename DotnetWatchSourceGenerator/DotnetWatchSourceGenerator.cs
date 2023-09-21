569fa34d2f1de9b4ef5d5ce20a86ed09f63c3289
Cyril Paulus <cyril.paulus@level-it.be>
2023-09-21 10:50:54 +0200

    Add project files.

---
+++ DotnetWatchSourceGenerator/DotnetWatchSourceGenerator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Threading;
+
+namespace DotnetWatchSourceGenerator
+{
+    // Thanks https://www.thinktecture.com/en/net/roslyn-source-generators-performance/
+    [Generator]
+    public class DotnetWatchSourceGenerator : IIncrementalGenerator
+    {
+        private static int _counter;
+
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var classProvider = context.SyntaxProvider
+                                       .CreateSyntaxProvider((node, _) =>
+                                       {
+                                           return node is ClassDeclarationSyntax;
+                                       },
+                                       (ctx, _) =>
+                                       {
+                                           return (ClassDeclarationSyntax)ctx.Node;
+                                       });
+
+            context.RegisterSourceOutput(classProvider, Generate);
+        }
+
+        private static void Generate(SourceProductionContext ctx, ClassDeclarationSyntax cds)
+        {
+            Generate(ctx, cds.Identifier.Text);
+        }
+
+        private static void Generate(SourceProductionContext ctx, string name)
+        {
+            var ns = "DemoConsoleApplication";
+
+            ctx.AddSource($"{ns}.{name}.perf.cs", $@"//
+
+// Counter: {Interlocked.Increment(ref _counter)}
+
+namespace {ns}
+{{
+   partial class {name}
+   {{
+   }}
+}}
+");
+        }
+    }
+}
